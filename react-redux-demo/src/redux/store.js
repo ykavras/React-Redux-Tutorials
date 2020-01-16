1ad943a830433a34e40ef8a6a5fbd4e9059cb4cc
Yasir KAVRAŞ <ykavras@gmail.com>
2020-01-16 22:37:36 +0300

    dispatch undefined
    logger must be the last middleware in chain, otherwise it will log thunk and promise, not actual actions.
    
    subject problem example : https://github.com/LogRocket/redux-logger/issues/302

--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -7,7 +7,7 @@ import rootReducer from './rootReducer'
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger, thunk))
+  composeWithDevTools(applyMiddleware(thunk, logger))
 )
 
 export default store
